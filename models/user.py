b1ede7c168e07cc43eec767077288194e4806288
elhocyn <houcinemfy1997@gmail.com>
2023-10-27 05:21:17 -0700

    README.md

---
+++ models/user.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+=======
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+>>>>>>> 98037fc2493150022b56f5ae8c0115fc2a16ff3c
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
+
+class User(BaseModel, Base):
+<<<<<<< HEAD
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+=======
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
+>>>>>>> 98037fc2493150022b56f5ae8c0115fc2a16ff3c
---
+++ web_flask/README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# web_flack
+=======
+0x02. AirBnB clone - MySQL
+
+>>>>>>> 98037fc2493150022b56f5ae8c0115fc2a16ff3c
